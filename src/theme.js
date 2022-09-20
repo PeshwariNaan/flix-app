50d2eccc653b6db3130259396505cc3da1a2d783
J.D. Browne <PeshwariNaan01@proton.me>
2022-09-20 16:23:52 +0400

    Added global theme and initial css styles.

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    :root {
+        --bg-main: #10141e;
+        --bg-ele: #161d2f;
+        --grey: #5a69af;
+        --red: #fc4747;
+        --white: #ffffff;
+        --body-med: 1.5rem;
+        --body-small: 1.3rem;
+    }
+`;
+
+
