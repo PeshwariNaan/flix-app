a7da6546b8fec4b697169b62caa6a2020446a94a
J.D. Browne <PeshwariNaan01@proton.me>
2022-10-07 12:38:59 +0400

    Added slider item component.

---
+++ src/components/flix-slider/slider-item/sliderItem.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    flex: 0 0 19.7%;
+    transition: transform 300ms ease 100ms;
+    margin: 0 .5rem;
+    position: relative;
+
+    img {
+        height: 100%;
+        width: 100%;
+        vertical-align: top;
+    }
+`
