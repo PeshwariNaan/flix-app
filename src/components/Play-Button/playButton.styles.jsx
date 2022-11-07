4cb4317f5f9f9597998c0027a0d7fac09421d579
J.D. Browne <PeshwariNaan01@proton.me>
2022-11-07 16:18:25 +0400

    Added play button component.

---
+++ src/components/Play-Button/playButton.styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledPlayButton = styled.button`
+  display: flex;
+  height: 5rem;
+  width: 11rem;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 5rem;
+  padding: 0 1rem;
+  cursor: pointer;
+  justify-content: space-around;
+  align-items: center;
+ 
+`;
