835c2c9a116e50b0c7963b357deb355e15454688
J.D. Browne <PeshwariNaan01@proton.me>
2022-11-11 15:52:25 +0400

    Modified ShowsContext to split up movies and shows for easier access to relative pages.

--- src/store/showContext.jsx
+++ src/store/showContext.jsx
@@ -1,48 +1,51 @@
 import React, { createContext, useState, useEffect } from 'react';
 
-import { addCollectionAndDocuments, getCategoriesAndDocuments } from '../utils/Firebase/firebase.utils';
+import {
+  addCollectionAndDocuments,
+  getCategoriesAndDocuments,
+} from '../utils/Firebase/firebase.utils';
 
-import SHOW_DATA from '../show-data'
-import SHOWS from '../shows.json'
+import SHOW_DATA from '../show-data';
+import SHOWS from '../shows.json';
 
 export const ShowContext = createContext({
-    // showsMap: {}
-    allShows: []
+  // showsMap: {}
+  allShows: [],
 });
 
 export const ShowProvider = ({ children }) => {
-    const [allShows, setAllShows] = useState([]);
-    const [shows, setShows] = useState(SHOWS)
+  const [allShows, setAllShows] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+  //const [shows, setShows] = useState(SHOWS);
 
-    // This was run once to populate the firestore db with our data file - SHOW_DATA// Here for demonstration only - no need to run after
-    // data has been uploaded once.
-    // useEffect(() => {
-    //     addCollectionAndDocuments('moviesAndShows', SHOW_DATA)
-    // }, [])
+  // This was run once to populate the firestore db with our data file - SHOW_DATA// Here for demonstration only - no need to run after
+  // data has been uploaded once.
+  // useEffect(() => {
+  //     addCollectionAndDocuments('moviesAndShows', SHOW_DATA)
+  // }, [])
 
-    useEffect(() => {
-        const getShowsArray = async () => {
-            const showsArray = await getCategoriesAndDocuments()
-            console.log(showsArray)
-            const allShowsArray = showsArray[0].items.concat(showsArray[1].items)
-            console.log('testArray', allShowsArray)
-            setAllShows(allShowsArray)
-        }
-        getShowsArray()
-    }, [])
+  useEffect(() => {
+    const getShowsArray = async () => {
+      const showsArray = await getCategoriesAndDocuments();
+      console.log(showsArray);
+      const allShowsArray = showsArray[0].items.concat(showsArray[1].items);
+      const moviesArray = showsArray[0].items;
+      const seriesArray = showsArray[1].items;
 
-  // const value = {    
-  //  showsMap
-  // };
+      console.log('testArray', allShowsArray);
+      setAllShows(allShowsArray);
+      setMovies(moviesArray);
+      setSeries(seriesArray);
+    };
+    getShowsArray();
+  }, []);
 
-const value = {
-    shows, 
-    allShows
-}
+  const value = {
+    allShows,
+    movies,
+    series,
+  };
 
-
-
-  return (
-    <ShowContext.Provider value={value}>{children}</ShowContext.Provider>
-  );
+  return <ShowContext.Provider value={value}>{children}</ShowContext.Provider>;
 };
