2f972a0310cd8a3eef25c2d1af59312fb86d40df
J.D. Browne <PeshwariNaan01@proton.me>
2022-11-11 11:00:02 +0400

    Changed context to use functions from firebase.utils to add and fetch data.

--- src/store/showContext.jsx
+++ src/store/showContext.jsx
@@ -1,18 +1,46 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
-import DATA from '../data.json'
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from '../utils/Firebase/firebase.utils';
+
+import SHOW_DATA from '../show-data'
+import SHOWS from '../shows.json'
 
 export const ShowContext = createContext({
+    // showsMap: {}
     allShows: []
 });
 
 export const ShowProvider = ({ children }) => {
-    const [allShows, setAllShows] = useState(DATA);
-    
+    const [allShows, setAllShows] = useState([]);
+    const [shows, setShows] = useState(SHOWS)
 
-  const value = {    
-   allShows
-  };
+    // This was run once to populate the firestore db with our data file - SHOW_DATA// Here for demonstration only - no need to run after
+    // data has been uploaded once.
+    // useEffect(() => {
+    //     addCollectionAndDocuments('moviesAndShows', SHOW_DATA)
+    // }, [])
+
+    useEffect(() => {
+        const getShowsArray = async () => {
+            const showsArray = await getCategoriesAndDocuments()
+            console.log(showsArray)
+            const allShowsArray = showsArray[0].items.concat(showsArray[1].items)
+            console.log('testArray', allShowsArray)
+            setAllShows(allShowsArray)
+        }
+        getShowsArray()
+    }, [])
+
+  // const value = {    
+  //  showsMap
+  // };
+
+const value = {
+    shows, 
+    allShows
+}
+
+
 
   return (
     <ShowContext.Provider value={value}>{children}</ShowContext.Provider>
