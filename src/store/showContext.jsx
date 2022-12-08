8629363cf596f1fe2c0d31afadf7583f3a74384c
J.D. Browne <PeshwariNaan01@proton.me>
2022-12-08 16:25:34 +0100

    Changed logic over to a reducer. Fetching data is working.

--- src/store/showContext.jsx
+++ src/store/showContext.jsx
@@ -1,35 +1,83 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useReducer } from 'react';
 
 import {
   addCollectionAndDocuments,
   getCategoriesAndDocuments,
 } from '../utils/Firebase/firebase.utils';
 
-import SHOW_DATA from '../show-data';
-import SHOWS from '../shows.json';
+// import SHOW_DATA from '../show-data';
+// import SHOWS from '../shows.json';
 
 export const ShowContext = createContext({
   // showsMap: {}
   allShows: [],
 });
 
+const SHOW_ACTION_TYPES = {
+  GET_ALL_SHOWS_REQUEST: 'GET_ALL_SHOWS_REQUEST',
+  GET_ALL_SHOWS_SUCCESS: 'GET_ALL_SHOWS_SUCCESS',
+  GET_ALL_SHOWS_FAIL: 'GET_ALL_SHOWS_FAIL',
+  GET_MOVIES: 'GET_MOVIES',
+  GET_SERIES: 'GET_SERIES',
+  GET_BOOKMARKED:'GET_BOOKMARKED',
+}
+
+const initialState = {
+  loading: false,
+  allShowsTotal: [],
+}
+
+const showReducer = (state, action) => {
+  switch(action.type){
+    case SHOW_ACTION_TYPES.GET_ALL_SHOWS_REQUEST:
+      return {
+        loading: true,
+        allShowsTotal: []
+      }
+    case SHOW_ACTION_TYPES.GET_ALL_SHOWS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        allShowsTotal: action.payload
+      }
+      case SHOW_ACTION_TYPES.GET_ALL_SHOWS_FAIL:
+        return {
+          loading: false,          
+        }
+      default:
+      return state;
+  }
+}
+
 export const ShowProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(showReducer, initialState);
   const [allShows, setAllShows] = useState([]);
   const [movies, setMovies] = useState([]);
   const [series, setSeries] = useState([]);
   const [bookmarkedShows, setBookmarkedShows] = useState([])  
 
+  
   // This was run once to populate the firestore db with our data file - SHOW_DATA// Here for demonstration only - no need to run after
   // data has been uploaded once.
   // useEffect(() => {
   //     addCollectionAndDocuments('moviesAndShows', SHOW_DATA)
   // }, [])
 
-  useEffect(() => {
-    const getShowsArray = async () => {
-      const showsArray = await getCategoriesAndDocuments('moviesAndShows');
-      
+  const value = {
+    allShowsTotal: state.allShowsTotal,
+    loading: state.loading,
+    allShows,
+    movies,
+    series,
+    bookmarkedShows
+  };
+
+  const fetchShows = async() => {
+    dispatch({type: SHOW_ACTION_TYPES.GET_ALL_SHOWS_REQUEST})
+    try {
+      const showsArray = await getCategoriesAndDocuments('moviesAndShows')
       const allShowsArray = showsArray[0].items.concat(showsArray[1].items);
+      dispatch({type: SHOW_ACTION_TYPES.GET_ALL_SHOWS_SUCCESS, payload: allShowsArray}) 
       const moviesArray = showsArray[0].items;
       const seriesArray = showsArray[1].items;
       setAllShows(allShowsArray);
@@ -44,16 +92,18 @@ export const ShowProvider = ({ children }) => {
       })
       setBookmarkedShows(bookmarkedShows)
       console.log(bookmarkedShows)
-    };
-    getShowsArray();
+      
+    }catch(error) {
+      dispatch({type: SHOW_ACTION_TYPES.GET_ALL_SHOWS_FAIL}) 
+      console.log('error getting data', error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchShows()
   }, []);
 
-  const value = {
-    allShows,
-    movies,
-    series,
-    bookmarkedShows
-  };
+ 
 
   return <ShowContext.Provider value={value}>{children}</ShowContext.Provider>;
 };
